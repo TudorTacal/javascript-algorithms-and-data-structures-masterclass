7ba356a5ed2b6b76344f6b366523c0d01260dbf6
TudorTacal <tudortacal@gmail.com>
2019-05-28 23:19:59 +0100

    areThereDuplicates Solution (Multiple Pointers) - a set can be used to solve the same problem

--- multiple pointers/are-there-duplicates.js
+++ multiple pointers/are-there-duplicates.js
@@ -1,13 +1,7 @@
 function areThereDuplicates(...args) {
   // sorting the array will take us out of O(n) realm
-  // but still
-  args.sort();
-  let i = 0;
-  for (let arg of args) {
-    if (arg === args[i+1]) return true;
-    i++;
-  }
-  return false;
+  //  check if an element is equal with the element at the next position
+  return args.sort().some((el, index) => el === args[index+1]);
 };
 
 module.exports.default = areThereDuplicates;
