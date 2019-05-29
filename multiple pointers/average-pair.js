8ac38fefd74006f899177433e93ff969c27a3da3
TudorTacal <tudortacal@gmail.com>
2019-05-29 23:16:21 +0100

    refactor averagePair to a more scalable solution

--- multiple pointers/average-pair.js
+++ multiple pointers/average-pair.js
@@ -1,16 +1,20 @@
 function averagePair(integers, average) {
-  // iterate thorough integers
-  // add every integer with the next
-  // calculate the average
-  // compare it to the given average
-  // return true if we find one
-  if (integers.some((int, index) => checkAverage(integers, int, average, index, 1))) return true;
-  if (integers.some((int, index) => checkAverage(integers, int, average, index, 2))) return true;
+  // iterate thorugh the array
+  // take 2 pointers, 1 at beginning, 1 at the end
+  // if i + j / 2 > average decrease j
+  // else increase i
+  let i = 0, j = integers.length - 1;
+  for (let k = 0; k < integers.length; k++) {
+    if ((integers[i] + integers[j]) / 2 > average) {
+      j--;
+    } else if ((integers[i] + integers[j]) / 2 < average) {
+      i++;
+    } else {
+      return true;
+    }
+  }
   return false;
+
 }
-
-const checkAverage = (integers, int, average, index, increment) =>
-  int + integers[index+increment] / 2 === average
-
 
 module.exports.default = averagePair;
--- multiple pointers/average-pair.test.js
+++ multiple pointers/average-pair.test.js
@@ -5,6 +5,4 @@ test('averagePair', () => {
   expect(averagePair([1, 3, 3, 5, 6, 7, 10, 12, 19], 8)).toEqual(true);
   expect(averagePair([-1, 0, 3, 4, 5, 6], 4.1)).toEqual(false);
   expect(averagePair([], 4)).toEqual(false);
-
-
 });
