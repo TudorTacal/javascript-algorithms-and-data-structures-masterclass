dbb42277d779b6b37b51f5816e4064bf7b84f207
TudorTacal <tudortacal@gmail.com>
2019-07-22 23:20:31 +0100

    reafactor/search/naiveStringSearch

--- search/naiveStringSearch.js
+++ search/naiveStringSearch.js
@@ -1,15 +1,10 @@
 function naiveStringSearch(str, substr) {
   let counter = 0;
   for (let i = 0; i < str.length; i++) {
-    let found = '';
     for (let j = 0; j < substr.length; j++) {
-      if (str[i + j] === substr[j]) {
-        found += str[i + j];
-      } else {
-        break;
-      }
+      if (str[i + j] !== substr[j]) break;
+      if (j === substr.length - 1) counter++;
     }
-    if (found === substr) counter++;
   }
   return counter;
 };
