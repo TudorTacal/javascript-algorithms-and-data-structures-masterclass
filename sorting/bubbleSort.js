8cbd6d1eae7b00e047c5da11eaaa96f951764a92
TudorTacal <tudortacal@gmail.com>
2019-07-30 21:47:32 +0100

    optimise bubbleSort

--- sorting/bubbleSort.js
+++ sorting/bubbleSort.js
@@ -4,12 +4,15 @@ function swap(arr, index1, index2) {
 }
 
 function bubbleSort(arr) {
+  let noSwaps = true;
   for (let i = arr.length - 1; i >= 0; i--) {
     for (let j = 0; j <= i - 1; j++) {
       if (arr[j] > arr[j+1]) {
         swap(arr, j, j+1);
+        noSwaps = false;
       }
     }
+    if (noSwaps) break;
   }
   return arr;
 }
