eac61857527f19aa69d94582d0b094035b71c44d
TudorTacal <tudortacal@gmail.com>
2019-09-02 12:24:44 +0100

    implement quickSort

--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -1,4 +1,10 @@
-function quickSort(arr) {
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr, left, pivotIndex - 1);
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
 }
 
 // return the index of where the element should be in the sorted array
